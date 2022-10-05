659c53942dbd7e4331e59997b3481b8773ad8666
PatrickKibe <phantickibe01@gmail.com>
2022-10-05 20:51:50 +0300

    initial commit

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,16 @@
+class PlantsController < ApplicationController
+    def index
+        plants = Plant.all
+        render json: plants
+    end
+
+    def show
+        plants = Plant.find_by(id:params[:id])
+        render json:plants
+    end
+
+    def create
+        plant = Plant.create(name: params[:name], image: params[:image], price: params[:price])
+        render json: plant, status: :created
+    end
+end
